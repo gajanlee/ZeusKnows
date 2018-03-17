3983381439535c699c19ec7007b6988f6a8162c4
www <w@w.com>
2018-03-17 11:16:52 +0800

    update preprocess

--- preprocess.py
+++ preprocess.py
@@ -42,22 +42,28 @@ def vocab_reader(filepaths, start_line=0, end_line=None):
 
 import re
 def filter(word):
-    return True if re.findall(re.compile(r'[a-zA-Z0-9]'), word) else False
+    return True if re.findall(re.compile(r'[a-zA-Z0-9]'), word) or len(word) > 8 else False
     """for ch in list(word):
-        if ch.isalpha() or ch.isalnum():
+ `       if ch.isalpha() or ch.isalnum():
             return True
     return False""" # bug, chinese characters are also 'alpha'
 
+max_paragraph = max_answer = 0
+mean_paragraph = mean_answer = 0
+word_list = []
 def preprocess_word_list(data):
     # default value, some documents no answers
-    max_paragraph = max_answer = 0
-    mean_paragraph = mean_answer = 0
+    global max_paragraph, max_answer
+    global mean_paragraph, mean_answer
     for doc in data["documents"]:
         para_len = 0
         for para in doc["segmented_paragraphs"]:
+            temp_word_list = []
             for word in para:
                 process(word)
+                temp_word_list.append(WordID(word))
             para_len += len(para)
+            word_list.append(temp_word_list)
         mean_paragraph = m_par.send(para_len)
         if para_len > max_paragraph: max_paragraph = para_len
     for answer in data["segmented_answers"]:
@@ -65,7 +71,7 @@ def preprocess_word_list(data):
             process(word)
         mean_answer = m_ans.send(len(answer))
         if len(answer) > max_answer: max_answer = len(answer)
-    return mean_paragraph, max_paragraph, mean_answer, max_answer
+    return max_paragraph, mean_paragraph, max_answer, mean_answer
         
 
 word_dict = {'<unknown>': Word(0, '<unknown>')}
@@ -76,7 +82,7 @@ mean_word_len, max_word_len = 0, 0
 long_word_list = []
 def process(word):
     global id, char_id, mean_word_len, max_word_len
-    if len(word) > 8 or filter(word): return
+    if filter(word): return
     for char in word:
         if char not in char_dict:
             char_dict[char] = char_id
@@ -92,6 +98,12 @@ def process(word):
     #if len(word) > 8 and word not in long_word_list:
     #    long_word_list.append(word)
     mean_word_len = m_word.send(len(word))
+
+def WordID(word):
+    if word in word_dict:
+        return word_dict[word].id
+    else:
+        return 0
 
 def statistics(max_paragraph, mean_paragraph, max_answer, mean_answer):
     with open("stat.log", "w") as f:
@@ -111,10 +123,14 @@ def main():
     statistics(*vocab_reader(["../Dureader/data/preprocessed/trainset/zhidao.train.json"], end_line=None))
     with open("word.dict", "w") as f:
         for word in sort_dict(word_dict):
-            f.write("%s %s %s, %s\n" % (word.id, word.content, word.count, word.length))
+            f.write("%s %s %s %s\n" % (word.id, word.content, word.count, word.length))
     with open("char.dict", "w") as f:
         for char, id in char_dict.items():
             f.write("%s %s\n" % (char, id))
+    with open("word_list.dict", "w") as f:
+        for wlst in word_list:
+            #print(wlst)
+            f.write("%s\n" % ' '.join(map(str, wlst)))
 
 def sort_dict(dict_words):
     """
