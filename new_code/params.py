508952cdaec1195bf837d82467c9882cfe70af86
lee <lee_jiazh@163.com>
2018-03-21 20:10:50 +0800

    refactor code

---
+++ new_code/params.py
@@ -0,0 +1,9 @@
+class Params:
+    
+    train_files = [""]
+
+    char_dict_path = "./word.txt"
+    wordlst_path = "./word_list.dict"   # save words' list
+    vocab_path = "./vocab.dict"
+    char_path = "./char.dict"
+    
