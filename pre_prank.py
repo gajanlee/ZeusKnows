52359daf1da737462b424d50c238b2afc432cb9b
xiaoniu <xiaoniu>
2018-03-30 16:34:55 +0800

    finish pre_prank, next fasttext tag

--- pre_prank.py
+++ pre_prank.py
@@ -9,39 +9,41 @@ class PassageTag:
         # self.positive, self.negative = [], []
 
     def _pro_data(self, d_json):
-        if not d_json["match_scores"]: return
+        if not d_json["match_scores"] or (len(d_json["segmented_question"]) == 0): return
         for doc in d_json["documents"]:
             if not doc["is_selected"]:
                 self.attr[self.__NEGATIVE].extend(
                     [{
                         "segmented_paragraph": seg_par,
-                        "segmented_question": doc["segmented_question"],
+                        "segmented_question": d_json["segmented_question"],
                         "tag": self.__NEGATIVE,
                         "char_paragraph": [[char for char in word] for word in seg_par],
-                        "char_question": [[char for char in word] for word in doc["segmented_question"]],
+                        "char_question": [[char for char in word] for word in d_json["segmented_question"]],
                     } for seg_par in doc["segmented_paragraphs"]]
                 )
             elif doc["is_selected"] and len(doc["segmented_paragraphs"]) == 1:
                 self.attr[self.__POSITIVE].extend(
                     [{
                         "segmented_paragraph": seg_par,
-                        "segmented_question": doc["segmented_question"],
+                        "segmented_question": d_json["segmented_question"],
                         "tag": self.__POSITIVE,
                         "char_paragraph": [[char for char in word] for word in seg_par],
-                        "char_question": [[char for char in word] for word in doc["segmented_question"]],
+                        "char_question": [[char for char in word] for word in d_json["segmented_question"]],
                     } for seg_par in doc["segmented_paragraphs"]]
                 )
 
     def start(self, filename=Params.train_files[0]):
         with open(filename) as f:
-            for i, l in enumerate(f):
-                if i == 100: break
+            for i, l in enumerate(f, 1):
+                if i % 1000 == 0: logger.info("%s / 90000" % i )
                 self._pro_data(json.loads(l))
         self.save()
 
     def save(self):
         with open("tag.stat", "w") as f, open("tag_id.stat", "w") as f_id:
-            [f.write("\n".join([json.dumps(attr) for attr in attrs])) for attrs in self.attr]
+            import random
+            self.attr[self.__NEGATIVE] = list(filter(lambda x: random.randint(1, 8) <= 2, self.attr[self.__NEGATIVE]))
+            [f.write("\n".join([json.dumps(attr, ensure_ascii=False) for attr in attrs])) for attrs in self.attr]
 
             for attrs in self.attr:
                 for attr in attrs:
