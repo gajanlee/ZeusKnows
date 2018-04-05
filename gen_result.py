14096b73676315830f239e0e0f962afdff5b69ad
xiaoniu <xiaoniu>
2018-04-05 15:59:41 +0800

    add gen and result.json

---
+++ gen_result.py
@@ -0,0 +1,33 @@
+import json
+
+lookup = json.load(open("total_lookup.stat"))
+
+writer = open("result.json", "w")
+#writerh = open("result_h.json", "w")
+print(lookup["301121"]["paragraphs"]["58075"])
+output = {}
+output_h = {}
+for tp in ["yes_no", "entity", "description"]:
+    with open("{}_test_res.stat".format(tp)) as fp:
+        for u, line in enumerate(fp, 1):
+            data = json.loads(line)
+            s1, s2 = data["spans"]
+            if s1 == s2: continue
+            if "".join(lookup[str(data["question_id"])]["paragraphs"][str(data["passage_id"])][s1: s2]) in ["。", "."]: continue
+            if lookup[str(data["question_id"])]["paragraphs"][str(data["passage_id"])][s1] == "。": s1 += 1
+            if data["question_id"] not in output:
+                output[data["question_id"]] = {
+                    "question_id": data["question_id"],
+                    "question_type": lookup[str(data["question_id"])]["question_type"],
+                    "answers": ["".join(lookup[str(data["question_id"])]["paragraphs"][str(data["passage_id"])][s1: s2]) ],
+                    "yesno_answers": [],
+                }
+            else:
+                output[data["question_id"]]["answers"].append(
+                    "".join(lookup[str(data["question_id"])]["paragraphs"][str(data["passage_id"])][s1: s2])
+                )
+            if u % 100 == 0: print(u)
+
+for o in output.values():
+    writer.write(json.dumps(o, ensure_ascii=False) + "\n")
+writer.close()
--- gen_upload.py
+++ gen_upload.py
@@ -1,37 +1,20 @@
 import json
-class QPair:
-    def __init__(self):
-        self.info = {}
-        for mode in ["yes_no", "entity", "description"]:
-            with open("{}_rank_test.stat".format(mode)) as f:
-                for line in f:
-                    data = json.loads(line)
-                    ap = {
-                        "passage_id": data["passage_id"],
-                        "question_type": data["question_type"],
-                        "segmented_paragraph": data["segmented_paragraph"]
-                    }
-                    if data["question_id"] not in self.info:
-                        self.info[data["question_id"]] = [ap]
-                    else:
-                        self.info[data["question_id"]].append(ap)
-        
-        self.res = {}
-        fw = open("upload_res.json", "w")
-        with open("test_res.stat") as f:
-            for i, line in enumerate(f):
-                data = json.loads(line)
-                for que in self.info[data["question_id"]]:
-                    if que["passage_id"] == data["passage_id"]:
-                        if data["question_id"] not in self.res:
-                            fw.write(json.dumps({
-                                "question_id": data["question_id"],
-                                "question_type": que["question_type"],
-                                "answers": ["".join(que["segmented_paragraph"][data["spans"][0]: data["spans"][1]+1])],
-                                "yesno_answers": [],
-                            }, ensure_ascii=False) + "\n")
-        fw.close()
 
-if __name__ == "__main__":
-    QPair()
-            
+lookup = {}
+with open("total_test.stat") as t:
+    for i, line in enumerate(t, 1):
+        data = json.loads(line)
+        if data["question_id"] in lookup:
+            lookup[data["question_id"]]["paragraphs"][data["passage_id"]] = data["segmented_p"]
+        else:
+            lookup[data["question_id"]] = {
+                "paragraphs": {data["passage_id"]: data["segmented_p"]},
+                "question": data["segmented_q"],
+                "question_id": data["question_id"],
+                "question_type": data["question_type"],
+            }
+        if i % 100 == 0: print(i) 
+
+f = open("total_lookup.stat", "w")
+f.write(json.dumps(lookup, ensure_ascii=False) + "\n")
+f.close()
