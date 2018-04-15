77ccbb2849beeb6b558d14e715be6b6ef9b02ddc
lee <lee_jiazh@163.com>
2018-04-15 22:46:21 +0800

    complete syn.py, untest

--- syn.py
+++ syn.py
@@ -1,5 +1,11 @@
+from .utils import *
 import json
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-v")
+parser.add_argument("-debug")
+args = parser.parse_args()
 
 
 class Config:
@@ -10,75 +16,119 @@ class Config:
         def check():
             assert Config.ensemble == True
             return func()
-        return inner
+        return check
 
 class DataHandler:
     """
     Contains All testing data, 
     """
-    def __init__(self, ensemble=False):
-        self.load_test_data()
-        if ensemble: self.load_bidaf_result()
+    def __init__(self):
+        self.lookup = {}    # It is used to find passage and question for "R-net" model
+        self.r_net_result = {}     # It stores R-net model result (after synthesis).
 
+        self.load_test_data("./test.net.json")
+        self.load_test_result("./res/14")
+        self.load_bidaf_result("../../07result.json")
+
+    @logging_util
     def load_test_data(self, path):
         """
             Load Data, testing data
         """
         with open(path) as f:
-            self.data = [json.loads(line) for line in f]
+            for i, line in enumerate(f, 1):
+                data = json.loads(line)
+                if data["question_id"] not in self.lookup:
+                    del data["segmented_question"], data["segmented_paragraph"]
+                    del data["char_question"], data["char_paragraph"]
+                    p_id, p = data["passage_id"], data["segmented_p"]
+                    del data["segmented_p"], data["passage_id"]
+                    data["passages"] = { p_id: p}
+                    self.lookup[data["question_id"]] = data
+                else:
+                    self.lookup[data["question_id"]]["passages"][data["passage_id"]] = data["segmented_p"]
+                if args.v and i % 1000 == 0: logger.info("Now Line %s" % i)
     
-    def load_bidaf_result(self):
-        with open("../../07result.json") as r:
-            self.bidaf_result = { json.loads(line)["question_id"]: json.loads(line) for line in f }
+    @logging_util
+    def load_test_result(self, path):
+        """
+        Load R_net Test Result, Recover R-net output to origin json.
+        :path: R-net output json file path.
 
+        # Example
+        path file contains R-net result;
+        {"passage_id": 1, "question_id": 0, answer_spans: [0, 1]}
 
-    def lookup_by_passage_id(self, passage_id):
-        return self.data[passage_id]
-                
+        :construct self.r_net_result
+        {"question_id": 0, "answer": [token1, token2, ...], "question": [token1, token2, ...]}
+        """
+        with open(path) as f:
+            for i, line in enumerate(f, 1):
+                d = json.loads(line)
+                ans = self.lookup[d["question_id"]]["passages"][d["passage_id"]][d["spans"][0]: d["spans"][1]]
+                if d["question_id"] not in self.r_net_result:
+                    self.r_net_result[d["question_id"]] = {
+                        "question_id": d["question_id"],
+                        "question_type": self.lookup[d["question_id"]]["question_type"],
+                        "question": self.lookup[d["question_id"]]["question"],
+                        "answers": ["".join(ans)],
+                        "yesno_answers": [],
+                        "entity_answers": [[]],
+                    }
+                else: 
+                    self.r_net_result[d["question_id"]]["answers"].append("".join(ans))
+                if args.v and i % 1000 == 0: logger.info("Now Line %s" % i)
+                if args.debug and i == 200: logger.info("Debug Model, Loaded R-net Data Done"); break
+
+        
+    @logging_util
+    def load_bidaf_result(self, path):
+        with open(path) as r:
+            self.bidaf_result = { json.loads(line)["question_id"]: json.loads(line) for line in r }
+
     #@Config.required_ensemble
     def get_all_bidaf(self):
         return self.bidaf_result
-    
-data_handler = DataHandler(ensemble=True)
 
-def recover_from_rnet(data_json):
+    def get_bidaf_by_id(self, id):
+        return self.bidaf_result[id]
+    
+data_handler = DataHandler()
+
+
+def write(result):
     """
-        :data_json: R-net output json.
-
-        # Example
-        {"passage_id": 1, "question_id": 0, answer_spans: [0, 1]}
-
-        :return
-        {"question_id": 0, "answer": [token1, token2, ...], "question": [token1, token2, ...]}
+    TODO:
+        Write Final Answers to a File.
     """
-    d = data_handler.lookup_by_passage_id(data_json["passage_id"])
-    return {
-        "question_id": d["question_id"],
-        "question_type": d["question_type"],
-        "answer": d["segmented_p"][d["spans"][0]: d["spans"][1]],
-        "question": d["question"]
-    }
+    with open("15r_net.json", "w") as w:
+        [w.write(json.dumps(d) + "\n") for d in result]
 
-def get_all_results():
-    results = {}
-    with open("r_net output") as f:
-        for line in f:
-            d = recover_from_rnet(json.loads(line))
-            if d["question_id"] in results:
-                results[d["question_id"]].answers.append( d["answer"])
-            else:
-                results[d["question_id"]] = {
-                    "question_id" : d["question_id"],
-                    "question_type" : d["question_type"],
-                    "answers" : [d["answer"]],
-                    "question" : d["question"],}
+
+def _get_best(*ans_jsons):
+    """
+    ans_jsons is a set of candidate data
+    """
+    anses = []
+    for ans_json in ans_jsons:
+        [anses.append(ans) for ans in ans_json["answers"] if len(ans) != 0]
+        # anses.extend(ans_json["answers"])
+
+    scores = []
+    for i, a1 in enumerate(anses):
+        scr = 0
+        for j, a2 in enumerate(anses):
+            if i == j: continue
+            scr += score(a1, a2)
+        scores.append((scr, a1))
+    scores.sort(key=lambda x: x[0], reverse=True)
+    if args.debug:
+        print(scores, "\n")
     
-    if Config.ensemble:
-        for question_id, data in data_handler.get_all_bidaf().items():
-            if question_id in results: results.extend(data["answers"])   # all data in it
-            else: results[question_id] = data
+    ans_jsons[0]["answers"] = [scores[0]]
+    return ans_jsons[0]
     
-    return results
+        
 
 def rerank(result):
     """
@@ -86,17 +136,17 @@ def rerank(result):
         get all result file ( if ensemble, contains all answer)
         assign final answer to result
     """
-    pass
+    res = {}
+    for q_id, ans in result.items():
+        res[q_id] = _get_best(ans, data_handler.get_bidaf_by_id(q_id))
+    
+    for q_id, d in data_handler.get_all_bidaf().items():
+        if q_id not in res: res[q_id] = d
+    write(res)
 
-def write():
-    """
-    TODO:
-        Write Final Answers to a File.
-    """
-    pass
 
 if __name__ == "__main__":
-    
+    rerank(data_handler.r_net_result)
     
 
 
--- utils.py
+++ utils.py
@@ -1,1 +1,20 @@
+from rouge_metric.rouge import Rouge
 
+# 字符级别的分数判定
+rouge = Rouge()
+def score(s1, s2):
+    return rouge.calc_score([s1], [s2])
+
+import logging
+logger = logging.getLogger("synethsis answer")
+logging.basicConfig(level = logging.DEBUG, 
+                    format = "%(asctime)s : %(levelname)s  %(message)s",
+                    datefmt = "%Y-%m-%d %A %H:%M:%S")
+
+from functools import wraps
+def logging_util(func):
+    @wraps(func)
+    def with_logging(*args, **kwargs):
+        logger.info("Starting %s" % (func.__name__))
+        return func(*args, **kwargs)
+    return with_logging
