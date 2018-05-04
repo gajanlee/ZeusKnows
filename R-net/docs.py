fba17e5aecc86d4b8609f9819ad6f9aedecf78c4
lee_jiazh <lee_jiazh@163.com>
2018-05-04 22:10:35 +0800

    init threading, 3xtime

--- R-net/docs.py
+++ R-net/docs.py
@@ -10,24 +10,44 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+import threading
+import Queue
+
+docs = Queue.Queue()
 class Document:
     def __init__(self, *data):
         self.link, self.title, self.passage = data
 
+
+class MulThreadDownload(threading.Thread):
+    def __init__(self, url):
+        super(MulThreadDownload,self).__init__()
+        self.url = url
+
+    def run(self):
+        # the details is encoded by utf-8
+        detail_soup = BeautifulSoup(unicode(requests.get(self.url).content, "gb2312", "ignore"), "html5lib")   # 忽略非gb2312的编码
+        title = detail_soup.find(attrs={"class":"ask-title"}).text
+        best_answer = detail_soup.find(attrs={"class": "best-text"})
+        if best_answer is None: return    # 没有最佳答案
+        docs.put(Document(self.url, title, best_answer.text))
+
 def getDocs(sentence, n=5):
-    docs = []
+    docs.queue.clear()
     r = requests.get('https://zhidao.baidu.com/search?lm=0&rn=10&pn=0&fr=search&ie=gbk&word='+sentence)
     lst_soup = BeautifulSoup(unicode(r.content, "gb2312", "ignore"), "html5lib")
     items = lst_soup.find_all("dl", attrs={"class": "dl"})[:int(n*1.4)]
+    threads = []
     for item in items:
-        # the details is encoded by utf-8
-        detail_soup = BeautifulSoup(unicode(requests.get(item.dt.a["href"]).content, "gb2312", "ignore"), "html5lib")   # 忽略非gb2312的编码
-        title = detail_soup.find(attrs={"class":"ask-title"}).text
-        best_answer = detail_soup.find(attrs={"class": "best-text"})
-        if best_answer is None: continue    # 没有最佳答案
-        docs.append(Document(item.dt.a["href"], title, best_answer.text))
-    return docs
+        t = MulThreadDownload(item.dt.a["href"])
+        t.start()
+        #t.join()
+        threads.append(t)
+    for t in threads:
+        t.join()
+    return
 
 if __name__ == "__main__":
-    for doc in getDocs("浦发银行电话号码"):
-        print(doc.title, "\n", doc.passage)
+    getDocs("浦发银行电话号码")
+    while not docs.empty():
+        print(docs.get().title)
