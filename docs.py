0dc5adbdff5026b3b6149e97be43c837b4e1028b
lee_jiazh <lee_jiazh@163.com>
2018-05-05 10:35:51 +0800

    half, refacte docs.py

--- docs.py
+++ docs.py
@@ -13,18 +13,20 @@ sys.setdefaultencoding('utf-8')
 import threading
 import Queue
 
-docs = Queue.Queue()
+docs = Queue.Queue()    # threading safety
 class Document:
     def __init__(self, *data):
         self.link, self.title, self.passage = data
 
-
-class MulThreadDownload(threading.Thread):
+class ZhidaoThreadDownload(threading.Thread):
     def __init__(self, url):
-        super(MulThreadDownload,self).__init__()
+        super(ZhidaoThreadDownload,self).__init__()
         self.url = url
 
-    def run(self):
+    def download_docs(url):
+        pass
+
+    def run(self, keyword, n=5):
         # the details is encoded by utf-8
         detail_soup = BeautifulSoup(unicode(requests.get(self.url).content, "gb2312", "ignore"), "html5lib")   # 忽略非gb2312的编码
         title = detail_soup.find(attrs={"class":"ask-title"}).text
@@ -32,20 +34,35 @@ class MulThreadDownload(threading.Thread):
         if best_answer is None: return    # 没有最佳答案
         docs.put(Document(self.url, title, best_answer.text))
 
-def getDocs(sentence, n=5):
+def download_docs(url):
     docs.queue.clear()
-    r = requests.get('https://zhidao.baidu.com/search?lm=0&rn=10&pn=0&fr=search&ie=gbk&word='+sentence)
+    r = requests.get(url)
     lst_soup = BeautifulSoup(unicode(r.content, "gb2312", "ignore"), "html5lib")
-    items = lst_soup.find_all("dl", attrs={"class": "dl"})[:int(n*1.4)]
+    items = lst_soup.find_all("dl", attrs={"class": "dl"})[:7]
     threads = []
     for item in items:
-        t = MulThreadDownload(item.dt.a["href"])
+        t = ZhidaoThreadDownload(item.dt.a["href"])
         t.start()
         #t.join()
         threads.append(t)
     for t in threads:
         t.join()
-    return
+
+def get_docs(sentence, n=5):
+    docs.queue.clear()
+    download_docs('https://zhidao.baidu.com/search?lm=0&rn=10&pn=0&fr=search&ie=gbk&word=' + sentence)
+
+    r = requests.get('https://zhidao.baidu.com/search?lm=0&rn=10&pn=0&fr=search&ie=gbk&word=' + sentence)
+    lst_soup = BeautifulSoup(unicode(r.content, "gb2312", "ignore"), "html5lib")
+    items = lst_soup.find_all("dl", attrs={"class": "dl"})[:int(n*1.4)]
+    threads = []
+    for item in items:
+        t = ZhidaoThreadDownload(item.dt.a["href"])
+        t.start()
+        #t.join()
+        threads.append(t)
+    for t in threads:
+        t.join()
 
 if __name__ == "__main__":
     getDocs("浦发银行电话号码")
