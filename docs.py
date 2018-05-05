5a99434c8ae1829aee053411dbd76bf8f78cdcb7
lee_jiazh <lee_jiazh@163.com>
2018-05-05 17:50:10 +0800

    refacte multithreading done

--- docs.py
+++ docs.py
@@ -18,53 +18,52 @@ class Document:
     def __init__(self, *data):
         self.link, self.title, self.passage = data
 
-class ZhidaoThreadDownload(threading.Thread):
-    def __init__(self, url):
-        super(ZhidaoThreadDownload,self).__init__()
-        self.url = url
+class Downloader(object):
+    def __init__(self, keyword):
+        self.docs = Queue.Queue()
+        self.url = self.synthesis_url(keyword)        
+    
+    def start(self):
+        Docs = []
+        self.start_downloading()
+        while not self.docs.empty():
+            Docs.append(self.docs.get())
+        return Docs
 
-    def download_docs(url):
-        pass
+    def synthesis_url(self, keyword):
+        raise NotImplementedError
 
-    def run(self, keyword, n=5):
-        # the details is encoded by utf-8
-        detail_soup = BeautifulSoup(unicode(requests.get(self.url).content, "gb2312", "ignore"), "html5lib")   # 忽略非gb2312的编码
-        title = detail_soup.find(attrs={"class":"ask-title"}).text
-        best_answer = detail_soup.find(attrs={"class": "best-text"})
-        if best_answer is None: return    # 没有最佳答案
-        docs.put(Document(self.url, title, best_answer.text))
+    def start_downloading(self):
+        raise NotImplementedError
 
-def download_docs(url):
-    docs.queue.clear()
-    r = requests.get(url)
-    lst_soup = BeautifulSoup(unicode(r.content, "gb2312", "ignore"), "html5lib")
-    items = lst_soup.find_all("dl", attrs={"class": "dl"})[:7]
-    threads = []
-    for item in items:
-        t = ZhidaoThreadDownload(item.dt.a["href"])
-        t.start()
-        #t.join()
-        threads.append(t)
-    for t in threads:
-        t.join()
+class Zhidao_Downloader(Downloader):
+    def __init__(self, keyword):
+        super(Zhidao_Downloader, self).__init__(keyword)
+    
+    def synthesis_url(self, keyword):
+        return 'https://zhidao.baidu.com/search?lm=0&rn=10&pn=0&fr=search&ie=gbk&word=' + keyword
+    
+    def start_downloading(self):
+        list_soup = get_html_content_soup(self.url)
+        threads = []
+        for item in list_soup.find_all("dl", attrs={"class": "dl"})[:7]:
+            t = threading.Thread(target=self.download_one, args=(item.dt.a["href"], self.docs))
+            t.start()
+            threads.append(t)
+        for t in threads:
+            t.join()
 
-def get_docs(sentence, n=5):
-    docs.queue.clear()
-    download_docs('https://zhidao.baidu.com/search?lm=0&rn=10&pn=0&fr=search&ie=gbk&word=' + sentence)
+    @staticmethod
+    def download_one(url, docs):
+        item_soup = get_html_content_soup(url)
+        best_answer = item_soup.find(attrs={"class": "best-text"})
+        if best_answer is None: return
+        docs.put(Document(url, item_soup.find(attrs={"class":"ask-title"}).text, best_answer.text))
 
-    r = requests.get('https://zhidao.baidu.com/search?lm=0&rn=10&pn=0&fr=search&ie=gbk&word=' + sentence)
-    lst_soup = BeautifulSoup(unicode(r.content, "gb2312", "ignore"), "html5lib")
-    items = lst_soup.find_all("dl", attrs={"class": "dl"})[:int(n*1.4)]
-    threads = []
-    for item in items:
-        t = ZhidaoThreadDownload(item.dt.a["href"])
-        t.start()
-        #t.join()
-        threads.append(t)
-    for t in threads:
-        t.join()
+def get_html_content_soup(url):
+    return BeautifulSoup(unicode(requests.get(url).content, "gb2312", "ignore"), "html5lib")   # 忽略非gb2312的编码(ignore)
+
 
 if __name__ == "__main__":
-    getDocs("浦发银行电话号码")
-    while not docs.empty():
-        print(docs.get().title)
+    for doc in Zhidao_Downloader("浦发银行电话号码").start():
+        print(doc.title)
